7aafb0140d6aac81dbac41b3f41aada63a80ac8f
Gene Le <gene.gkl@gmail.com>
2014-07-04 22:20:37 -0500

    finish tut

--- js/views/todo.js
+++ js/views/todo.js
@@ -1,33 +1,20 @@
 todo.view = function(ctrl) {
-  return m("html", [
-    m("body", [
-      m("input", {onchange: m.withAttr("value", ctrl.description), value: ctrl.description()}),
-      m("button", "Add"),
-      m("table", [
-        m("tr", [
-          m("td", [
-            m("input[type=checkbox]")
-            ]), 
-          m("td", "task description"),
+    return m("html", [
+        m("body", [
+            m("input", {onchange: m.withAttr("value", ctrl.description), value: ctrl.description()}),
+            m("button", {onclick: ctrl.add}, "Add"),
+            m("table", [
+                ctrl.list.map(function(task, index) {
+                    return m("tr", [
+                        m("td", [
+                            m("input[type=checkbox]", {onclick: m.withAttr("checked", task.done), checked: task.done()})
+                        ]),
+                        m("td", {style: {textDecoration: task.done() ? "line-through" : "none"}}, task.description()),
+                    ])
+                })
+            ])
         ])
-      ])
-    ])
-  ]);
-}
+    ]);
+};
 
-// m.render(document, todo.view(ctrl));
-
-
-//////////////
-// Bindings //
-
-var ctrl = new todo.controller();
-
-
-ctrl.description(); // empty string
-m.render(document, todo.view(ctrl)); // input is empty
-ctrl.description("Write code"); //set the description in the controller
-m.render(document, todo.view(ctrl)); // input now says "Write code"
-
-
-
+m.module(document, todo);
